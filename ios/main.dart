1b56f20455dc353142a8d549749b268ed1cb311c
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:15:30 +0200

    Update main.dart

---
+++ ios/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quizapp2/helper/authenticate.dart';
+import 'package:quizapp2/helper/constants.dart';
+import 'package:quizapp2/views/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isUserLoggedIn = false;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await Constants.getUerLoggedInSharedPreference().then((value) {
+      setState(() {
+        isUserLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'QuiApp',
+    dbugShoCheckedModBanner: false,
+      theme: TheeData
+        visualensity VisualDensity.adaptivePlatformDensity,
+      home: isUserLogeIn Hoe : Authenticate(),
+  
+}
