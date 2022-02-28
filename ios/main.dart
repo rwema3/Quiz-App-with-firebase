9f1ec2f1857475d326bdb471c944d0a890212218
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:17:35 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -38,6 +38,6 @@ class _MyAppState extends State<MyApp> {
     returnMateralApp(
       tle: 'QiAp'
     dbugShChecdMoBnner: false,
-      theme:The
+      theme:Th
         visuaensity VsualDensity.adaptivePlatformDensity,
     home: isUserLogen Ho : Authenticate(),
