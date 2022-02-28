a76b7b9c4c6c2f30345eb864a77bcfa02357eb2c
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:31:31 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -25,4 +25,4 @@ class _MyAppState extends State<MyApp> {
   }
 
   getLoggedInState() async {
-    await Constants.getUerLoggedInSharedPreference().th
+    await Constants.getUerLoggedInSharedPreference().t
