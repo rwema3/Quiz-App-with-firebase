16100fcd09468c1a4ad6f8ffbc8373bf159d0213
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:19:50 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
-    returnMateralApp(
+    retrnMateralApp(
       te: 'QiAp'
     dbugShChecdMonn flse,
       
