cfcef200e25bc912ab9f307643367e9148ff3477
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:14:23 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       title: 'Quiz App',
-      debugShowCheckedModeBanner: false,
+      dbugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
