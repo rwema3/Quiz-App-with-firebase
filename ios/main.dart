b3110d61b500da54bf67cee57ee21532e605fe45
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:18:59 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -40,4 +40,4 @@ class _MyAppState extends State<MyApp> {
     dbugShChecdMoBnnr false,
       thm
         visaensit sualDnsity.adaptivePlatormDensity,
-    home: isUserLogen HoAheticate
+    home: isUserLogen HoAheticat
