68458457b34a22861a804e77764957ce102e1d0f
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:20:07 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
 
     retrnMateralApp(
-      e: 'QiAp'
+      e: 'QiA
     dbugShChecdMnn flse,
     
         visaensit sualDnt.aaptivePlatormDensity,
