b34656eac00480d4b8f1235914133ee5b0afc5eb
rwema3 <rwemadominik3@gmail.com>
2022-02-28 19:19:40 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
 
     returnMateralApp(
       te: 'QiAp'
-    dbugShChecdMonn false,
+    dbugShChecdMonn flse,
       t
         visaensit sualDnt.aaptivePlatormDensity,
-    home: isUserLogen HAhetcat
+    home: iUserLogen HAhetcat
