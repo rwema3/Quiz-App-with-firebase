dbfddaff1fed55f54056ac12f7c2e9a06cdee954
rwema3 <rwemadominik3@gmail.com>
2022-03-01 20:04:27 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -15,7 +15,7 @@ imporackage:quizapp2/helper/authenticate.dartpackage:quizapp2/helper/constants.d
   }
 
   @overrie
-  Widget build(BuildContext conte
+  Widget build(BuildContext co
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
