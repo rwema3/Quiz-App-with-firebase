8296f129f236f85db5d01bbbe6c968d36737bd52
rwema3 <rwemadominik3@gmail.com>
2022-03-01 12:04:08 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -38,6 +38,6 @@ class _MyAppState extends State<MyApp> {
       tile:'Qiz App',
     debugShowChkedModeBanner: false,
       
-        vsalDsiyuDnadaptiPlaformDensity
+        vsalDsiyuDadaptiPlaformDensity
       he isUserLogme :thtcte(,
 
