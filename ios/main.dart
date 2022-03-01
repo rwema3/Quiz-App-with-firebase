0f99b866dd7b0dbd514b2c9d3d9aec90740dd88c
rwema3 <rwemadominik3@gmail.com>
2022-03-01 19:46:36 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -12,7 +12,7 @@ void main() {
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  _MyAppState createState() => _
+  _MyAppState createState() => 
   void initState() {
     getLoggedInState();
     super.initState();
