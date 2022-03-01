f043ae89f3a956230b177a3f0df05885d78a5ed0
rwema3 <rwemadominik3@gmail.com>
2022-03-01 20:13:45 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
+import 'package:flutter/services.dart'
   getLoggedInState() async {
     await Constants.getUerLoggedInSharedPreference().then((value) {
       setState(() {
