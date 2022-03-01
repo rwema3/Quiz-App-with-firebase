68c5acc6ce0802dd7e38b8de5ec66782dd13cf1e
rwema3 <rwemadominik3@gmail.com>
2022-03-01 19:45:13 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>
+class _MyAppState extends State<
   void initState() {
     getLoggedInState();
     super.initState();
