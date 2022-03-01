b6e827cfce88b89ba819ddca81570451a5692857
rwema3 <rwemadominik3@gmail.com>
2022-03-01 20:13:03 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-imporackage:quizapp2/helper/
+imporackage:quizapp2/helpe
   getLoggedInState() async {
     await Constants.getUerLoggedInSharedPreference().then((value) {
       setState(() {
