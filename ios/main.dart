d2f88866824ef5c7a6296ed390c642b710e05ea8
rwema3 <rwemadominik3@gmail.com>
2022-03-01 19:54:41 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:quizapp2/helper/authenticate.dart';
 import 'package:quizapp2/helper/constants.dart';
 import 'package:quizapp2/views/home.dart';
-import 'package:shared
+import 'package:share
   void initState() {
     getLoggedInState();
     super.initState();
