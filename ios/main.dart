5f4d8de5b05946c9a5c433fd9462e4fde5131e68
rwema3 <rwemadominik3@gmail.com>
2022-03-01 21:07:38 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -8,7 +8,7 @@ import package:flutter/material.dartackage:flutter/services.dart
   }
 
   @overrie
-  Widget build(BuildC
+  Widget build(BuildCo
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeDa
