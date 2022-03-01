91033076fe2f8ef4489981d6fbfd71b21725acdd
rwema3 <rwemadominik3@gmail.com>
2022-03-01 19:44:11 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool isUserLoggedIn = false;
 
-  @
+  
   void initState() {
     getLoggedInState();
     super.initState();
