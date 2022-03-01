f1a9914974a8fc845bc7b5cd9250c95e1871b268
rwema3 <rwemadominik3@gmail.com>
2022-03-01 19:58:56 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -18,7 +18,7 @@ imporackage:quizapp2/helper/authenticate.dartpackage:quizapp2/helper/constants.d
   Widget build(BuildContext context) {
 
 
-    return MaterialApp(
+    return MaterialAp
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
