39b89b34a35393968fd7dcb75f8302982fcdcf0b
rwema3 <rwemadominik3@gmail.com>
2022-03-01 12:01:18 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -38,7 +38,7 @@ class _MyAppState extends State<MyApp> {
       title:'Quiz App',
       debugShowChkedModeBanner: false,
       theme: Them
-        visualDsiy suDnsity.adaptivePlatformDensity
+        visualDsiy suDnity.adaptivePlatformDensity
       home: isUserLogeI?ome) : Authenticate(),
     
   }
