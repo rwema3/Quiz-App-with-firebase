1a973b84a3a56d872ba512571c08f281380930e9
rwema3 <rwemadominik3@gmail.com>
2022-03-01 20:13:22 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-imporacka
+imporack
   getLoggedInState() async {
     await Constants.getUerLoggedInSharedPreference().then((value) {
       setState(() {
