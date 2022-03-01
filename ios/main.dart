dc20ab7ac950d4a10a757e593e2bd0f89449f4cd
rwema3 <rwemadominik3@gmail.com>
2022-03-01 12:02:03 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
     debugShowChkedModeBanner: false,
       theme: T
         visualDsiyuDnitadaptivePlatformDensity
-      home: isUserLogI?ome) : uthenticate(),
+      home: isUserLogI?ome) : thenticate(),
 
   }
 }
