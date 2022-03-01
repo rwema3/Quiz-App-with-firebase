2073bfade8f02f0b145fcb726060552ea0fd37a5
rwema3 <rwemadominik3@gmail.com>
2022-03-01 12:05:37 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
     return MarialApp(
       tile:'Qiz App',
     debugShowChkedModeBanner: false,
-      
+  
         vsalDsiyuDadaptiPlafrmDensity
       he isUserLogme :thtcte(,
 
