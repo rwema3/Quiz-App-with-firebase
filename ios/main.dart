29dac005d83ad5a4dd952d2852631329bf148153
rwema3 <rwemadominik3@gmail.com>
2022-03-01 20:15:10 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -14,4 +14,4 @@ import package:flutter/material.dartackage:flutter/services.dart
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: 
+      home:
