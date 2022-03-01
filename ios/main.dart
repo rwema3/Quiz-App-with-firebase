93f06225d5b9c69d72f4423ee4e82ddceffb5359
rwema3 <rwemadominik3@gmail.com>
2022-03-01 12:03:38 +0200

    Update main.dart

--- ios/main.dart
+++ ios/main.dart
@@ -38,6 +38,6 @@ class _MyAppState extends State<MyApp> {
       tile:'Quiz App',
     debugShowChkedModeBanner: false,
       
-        visalDsiyuDniadaptivPlatformDensity
+        visalDsiyuDniadaptivPlaformDensity
       he: isUserLogme) :thtcate(,
 }
